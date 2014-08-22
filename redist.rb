396ae9599683e99d20000c4a8b3288e08552bbff
David Newman <dnewman@schoology.com>
2014-08-22 18:02:31 -0400

    Working command line options.

--- redist.rb
+++ redist.rb
@@ -4,12 +4,67 @@ require 'rubygems'
 require 'hiredis'
 require 'redis'
 require 'date'
+require 'optparse'
+
+options = {}
+OptionParser.new do |opts|
+
+  opts.banner = "Usage: redist <migrate|expire|persist|del> [options]"
+
+  opts.on("--src-host HOST", "Source Redis host") do |src|
+    options[:src_host] = src
+  end
+
+  opts.on("--src-port PORT", String, "Source Redis port") do |src|
+    options[:src_port] = src
+  end
+
+  opts.on("--dst-host HOST", String, "Destination Redis host") do |src|
+    options[:dst_host] = src
+  end
+
+  opts.on("--dst-port PORT", String, "Destination Redis port") do |src|
+    options[:dst_port] = src
+  end
+
+  opts.on("--op OPERATION", String, "Operation to run") do |op|
+    options[:op] = op
+  end
+
+  opts.on("--expire-ttl [TTL]", Integer, "TTL when expiring keys") do |ttl|
+    options[:ttl] = ttl
+  end
+
+  opts.on("--log [LOGFILE]", String, "Path to log file") do |logfile|
+    options[:log] = logfile
+  end
+
+  opts.on("--progress [COUNT]", Integer, "Print progress message every COUNT keys") do |progress|
+    options[:progress] = progress
+  end
+
+end.parse!
+
+def validate_options(options)
+  if !options.has_key?(:op)
+    log("You must specify an operation (e.g. --op migrate)")
+    return false
+  elsif options[:op] == "expire" && !options.has_key?("ttl")
+    log("You must specify a TTL when using '--op expire'")
+    return false
+  end
+  return true
+end
 
 def log(message)
   ts = DateTime.now
-  f = open('redist.log', 'a')
-  f.puts "#{ts} - #{message}"
-  f.close()
+  msg = "#{ts} - #{message}" 
+  if options.has_key?(:log)
+    f = open(options[:log], 'a')
+    f.puts msg
+    f.close()
+  end
+  puts msg
 end
 
 def migrate(key, src, dst)
@@ -58,35 +113,42 @@ def persist(key, src, dst)
   end
 end
 
-src_host = '127.0.0.1'
-src_port = '6379'
-dst_host = '127.0.0.1'
-dst_port = '22121'
-#dst_port = '6379'
+def redist(opts)
+  src_host = '127.0.0.1'
+  src_port = '6379'
+  dst_host = '127.0.0.1'
+  dst_port = '22121'
+  #dst_port = '6379'
 
-total_keys = 0
-increment = 10000
-#op = expire
-
-log("starting processing")
-begin
-  db_keys = 0
-  db = 0
-  log("Starting Database: #{db}")
-  src = Redis.new(:host => src_host, :port => src_port, :db => db, :driver => :hiredis)
-  dst = Redis.new(:host => dst_host, :port => dst_port, :db => db, :driver => :hiredis)
+  total_keys = 0
+  increment = 10000
+  #op = expire 
+  log("starting processing")
+  begin
+    db_keys = 0
+    db = 0
+    log("Starting Database: #{db}")
+    src = Redis.new(:host => src_host, :port => src_port, :db => db, :driver => :hiredis)
+    dst = Redis.new(:host => dst_host, :port => dst_port, :db => db, :driver => :hiredis)
   
-  src.scan_each do |key|
-    persist(key, src, dst)
-    db_keys += 1
-    total_keys += 1
+    src.scan_each do |key|
+      persist(key, src, dst)
+      db_keys += 1
+      total_keys += 1
 
-    if db_keys % increment == 0
-      log("Processed #{db_keys} from Database #{db}")
-    end 
+      if db_keys % increment == 0
+        log("Processed #{db_keys} from Database #{db}")
+      end 
+    end
+    log("Done processing Database: #{db}")
+  rescue
+    log($!)
   end
-  log("Done processing Database: #{db}")
-rescue
-  log($!)
+  log("processing complete")
 end
-log("processing complete")
+
+if !validate_options()
+  puts "ERROR: Invalid options specified. Try 'redist -h'"
+else
+  #redist(options)
+end
